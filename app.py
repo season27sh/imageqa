106e58c934587eea82d7d21d2f168d1a07ee6276
season27sh <140854623+season27sh@users.noreply.github.com>
2026-07-11 17:02:50 +0530

    app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def answer_image(data: RequestData):
         image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.5-pro",
             contents=[
                 image,
                 f"""
