083f1461aa3b572ec66d15d9156e9add2962afbd
season27sh <140854623+season27sh@users.noreply.github.com>
2026-07-11 12:28:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from google import genai
+from PIL import Image
+import base64
+import io
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class RequestData(BaseModel):
+    image_base64: str
+    question: str
+
+@app.post("/answer-image")
+def answer_image(data: RequestData):
+
+    image_bytes = base64.b64decode(data.image_base64)
+    image = Image.open(io.BytesIO(image_bytes))
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            data.question,
+            image
+        ]
+    )
+
+    return {
+        "answer": response.text.strip()
+    }
